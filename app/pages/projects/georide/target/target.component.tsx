076637e5bd223f01a758fc7acbbecc1db23c6c37
Mathias Genibrel <mathias.geni@gmail.com>
2025-02-20 22:59:52 +0100

    chore(target): change wording

--- app/pages/projects/georide/target/target.component.tsx
+++ app/pages/projects/georide/target/target.component.tsx
@@ -1,21 +1,27 @@
 import { FC } from "react";
 import Heading from "~/common/components/heading/Heading";
+import { Sparkles } from "~/common/components/sparkles/sparkles.component";
+import Text from "~/pages/home/components/about-me/Text";
 
 interface TargetProps {}
 
 export const Target: FC<TargetProps> = ({}) => {
   return (
     <article className={"mx-auto max-w-4xl px-8 py-4"}>
-      <Heading.Secondary className={"pt-4 pb-2"}>
-        L'Objectif: Créer une Connexion Émotionnelle
-      </Heading.Secondary>
+      <Heading.Secondary className={"pt-4 pb-2"}>L'Objectif</Heading.Secondary>
       <p className={"text-lg"}>
-        Malgré une application techniquement performante, les utilisateurs
-        exprimaient un besoin clair : ils voulaient que leur moto soit mise en
-        avant, qu'elle devienne la star de leur écran. Ma mission était de
-        repenser l'interface pour créer cette connexion émotionnelle
-        instantanée, sans compromettre les fonctionnalités de sécurité qui font
-        l'ADN de GeoRide.
+        <Sparkles minSparkleDelay={300} maxSparkleDelay={1000}>
+          <i>"Avoir sa moto dans la poche"</i>
+        </Sparkles>{" "}
+        - cette phrase résume parfaitement l'attente des utilisateurs.
+        <br />
+        <Text.Dark>
+          Plus qu'une simple application de sécurité, ils voulaient ressentir
+          cette connexion unique avec leur machine.
+        </Text.Dark>{" "}
+        Ma mission était de transformer cette expérience utilisateur pour faire
+        de chaque moto la véritable star de l'application, sans compromis sur la
+        sécurité.
       </p>
     </article>
   );
