c75afefca8b5c209b71de8ed4c53a89cdb58183b
Mathias Genibrel <mathias.geni@gmail.com>
2025-02-25 14:41:15 +0100

    feat(warning-box): implement warning box component

---
+++ app/common/components/box/warning-box.component.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import { TriangleWarningIcon } from "~/common/components/icons/triangle-warning.icon";
+import clsx from "clsx";
+
+interface WarningBoxProps {
+  children: string;
+  className?: HTMLDivElement["className"];
+}
+
+export const WarningBox: FC<WarningBoxProps> = ({ children, className }) => {
+  return (
+    <div
+      className={clsx(
+        "flex items-center gap-2 rounded-lg border-2 border-yellow-300 bg-yellow-50 p-4 text-yellow-800",
+        className,
+      )}
+    >
+      <div
+        className={
+          "text-background flex h-9 w-9 items-center justify-center rounded-full bg-yellow-500 p-2"
+        }
+      >
+        <TriangleWarningIcon className={"h-5.5 w-5.5"} />
+      </div>
+      <span className={"font-display font-medium"}>{children}</span>
+    </div>
+  );
+};
